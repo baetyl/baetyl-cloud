343599b8695c7ec3b3d435218c680c360be1ae46
jiangxin <yibishufange@gmail.com>
2020-10-22 10:21:59 +0800

    helm安装配置修改 (#146)
    * helm安装配置修改
    
    * template文件归一
    
    * fix
    
    * fix

--- scripts/native/sql/data.sql
+++ scripts/native/sql/data.sql
@@ -2,11 +2,11 @@ INSERT INTO `baetyl_property` (`name`, `value`) VALUES
 ('sync-server-address', 'https://host.docker.internal:9005'),
 ('init-server-address', 'https://0.0.0.0:9003'),
 
-('baetyl-image', 'baetyltechtest/baetyl:git-84127d1'),
-('baetyl-function-image', 'hub.baidubce.com/baetyl/function:v2.0.0'),
-('baetyl-function-runtime-python3','hub.baidubce.com/baetyl/function-python:3.6-v2.0.0'),
-('baetyl-function-runtime-nodejs10','hub.baidubce.com/baetyl/function-node:10.19-v2.0.0'),
-('baetyl-function-runtime-sql','hub.baidubce.com/baetyl/function-sql:v2.0.0'),
+('baetyl-image', 'hub.baidubce.com/baetyl/baetyl:v2.1.0'),
+('baetyl-function-image', 'hub.baidubce.com/baetyl/function:v2.1.0'),
+('baetyl-function-runtime-python3','hub.baidubce.com/baetyl/function-python:3.6-v2.1.0'),
+('baetyl-function-runtime-nodejs10','hub.baidubce.com/baetyl/function-node:10.19-v2.1.0'),
+('baetyl-function-runtime-sql','hub.baidubce.com/baetyl/function-sql:v2.1.0'),
 ('baetyl-version-latest', 'v2.1.0'),
 
 ('command-docker-installation', 'curl -sSL https://get.daocloud.io/docker | sh'),
