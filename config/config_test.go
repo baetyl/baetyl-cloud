a11161134cc036eda1a809adf101b4bcc52c36c4
xiejingru <xiejingru@baidu.com>
2020-12-15 15:55:22 +0800

    add shadow fields (#203)

--- config/config_test.go
+++ config/config_test.go
@@ -49,6 +49,9 @@ func TestDefaultValue(t *testing.T) {
 	expect.Plugin.Property = "database"
 	expect.Plugin.SyncLinks = []string{"httplink"}
 	expect.Plugin.Pubsub = "defaultpubsub"
+	expect.Plugin.DeviceModel = "databaseext"
+	expect.Plugin.Device = "databaseext"
+	expect.Plugin.Driver = "databaseext"
 
 	expect.Template.Path = "/etc/baetyl/templates"
 
