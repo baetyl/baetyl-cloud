d81a1de0e4fbb1f8420d0f0f297e34c79b163938
yibishufange <yibishufange@gmail.com>
2021-02-04 16:47:24 +0800

    update node/app create time bug fix

--- api/node.go
+++ api/node.go
@@ -224,6 +224,7 @@ func (api *API) UpdateNode(c *common.Context) (interface{}, error) {
 	})
 	node.Version = oldNode.Version
 	node.Attributes = oldNode.Attributes
+	node.CreationTimestamp = oldNode.CreationTimestamp
 
 	err = models.PopulateNode(oldNode)
 	if err != nil {
