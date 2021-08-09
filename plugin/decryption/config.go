315c8a2d35dfc689ed0d9961df4da1029c7fb9bb
jiangxin <yibishufange@gmail.com>
2021-08-09 15:48:07 +0800

    mysql password sm4 decryption v2 (#343)
    * mysql password sm4 decryption v2
    
    * fix comment

--- plugin/decryption/config.go
+++ plugin/decryption/config.go
@@ -1,10 +1,12 @@
 package decryption
 
 type CloudConfig struct {
+	// CipherText 密码密文  IV sm4 cbc解密iv分量  Sm4ProtectKey sm4解密密钥保护密钥分量 Sm4EncKey sm4解密密钥加密分量
 	Decryption struct {
-		Type       string `yaml:"type" json:"type"`
-		CipherText string `yaml:"cipherText" json:"cipherText"`
-		IV         string `yaml:"iv" json:"iv"`
-		Sm4Key     string `yaml:"sm4Key" json:"sm4Key"`
+		Type          string   `yaml:"type" json:"type"`
+		CipherText    string   `yaml:"cipherText" json:"cipherText"`
+		IV            string   `yaml:"iv" json:"iv"`
+		Sm4ProtectKey []string `yaml:"sm4ProtectKey" json:"sm4ProtectKey"`
+		Sm4EncKey     string   `yaml:"sm4EncKey" json:"sm4EncKey"`
 	} `yaml:"decryption" json:"decryption" default:"{}"`
 }
--- plugin/decryption/decryption.go
+++ plugin/decryption/decryption.go
@@ -3,11 +3,12 @@ package decryption
 import (
 	"encoding/hex"
 
+	"github.com/ZZMarquis/gm/sm4"
+	"github.com/ZZMarquis/gm/util"
 	"github.com/baetyl/baetyl-cloud/v2/common"
 	"github.com/baetyl/baetyl-cloud/v2/plugin"
 	"github.com/baetyl/baetyl-go/v2/errors"
 	"github.com/baetyl/baetyl-go/v2/log"
-	"github.com/tjfoc/gmsm/sm4"
 )
 
 type Decryption struct {
@@ -35,35 +36,76 @@ func (d *Decryption) Decrypt(cipherText string) (string, error) {
 	var err error
 	switch d.cfg.Decryption.Type {
 	case "sm4":
-		url, err = d.sm4Decryption(cipherText, d.cfg.Decryption.Sm4Key, d.cfg.Decryption.IV)
+		url, err = d.sm4Decryption(cipherText, d.cfg.Decryption.Sm4EncKey, d.cfg.Decryption.IV, d.cfg.Decryption.Sm4ProtectKey)
 	default:
 		url = cipherText
 	}
 	return url, errors.Trace(err)
 }
 
-// 注意：sm4解密需提供密文及密钥，以hex string形式提供
-func (d *Decryption) sm4Decryption(cipherText, sm4Key, iv string) (string, error) {
+// 注意：sm4解密需提供密文、IV分量及密钥保护分量，密钥加密分量，以hex string形式提供
+func (d *Decryption) sm4Decryption(cipherText, sm4EncKey, iv string, sm4ProtectKey []string) (string, error) {
+	if cipherText == "" || sm4EncKey == "" || iv == "" || sm4ProtectKey == nil {
+		d.log.Error("failed to decrypt, not enough param provided")
+		return "", errors.Trace(errors.New("failed to decrypt, not enough param provided"))
+	}
 	decCipherText, err := hex.DecodeString(cipherText)
 	if err != nil {
-		d.log.Error("fail to decode cipherText", log.Any("cipherText", cipherText), log.Any("error", err))
-		return "", errors.Trace(err)
-	}
-	decKey, err := hex.DecodeString(sm4Key)
-	if err != nil {
-		d.log.Error("fail to decode sm4Key", log.Any("sm4Key", sm4Key), log.Any("error", err))
+		d.log.Error("failed to decode cipherText", log.Any("cipherText", cipherText), log.Any("error", err))
 		return "", errors.Trace(err)
 	}
 
 	decIV, err := hex.DecodeString(iv)
-	_ = sm4.SetIV(decIV)
-
-	plaintext, err := sm4.Sm4Cbc(decKey, decCipherText, false)
 	if err != nil {
-		d.log.Error("fail to decrypt cipherText", log.Any("decCipherText", decCipherText), log.Any("error", err))
+		d.log.Error("failed to decode iv", log.Any("iv", iv), log.Any("error", err))
 		return "", errors.Trace(err)
 	}
-	return string(plaintext), nil
+	var proKey []byte
+	for i, p := range sm4ProtectKey {
+		decP, err := hex.DecodeString(p)
+		if err != nil {
+			d.log.Error("failed to decode sm4ProtectKey", log.Any("sm4ProtectKey", sm4ProtectKey), log.Any("error", err))
+			return "", errors.Trace(err)
+		}
+		if i == 0 {
+			proKey = decP
+		} else {
+			proKey = XORBytes(proKey, decP)
+			if proKey == nil {
+				d.log.Error("failed to xor sm4ProtectKey", log.Any("error", err))
+				return "", errors.Trace(err)
+			}
+		}
+	}
+	encKey, err := hex.DecodeString(sm4EncKey)
+	if err != nil {
+		d.log.Error("failed to decode sm4EncKey", log.Any("sm4Key", sm4EncKey), log.Any("error", err))
+		return "", errors.Trace(err)
+	}
+
+	decKey, err := sm4.ECBDecrypt(proKey, encKey)
+	if err != nil {
+		d.log.Error("failed to decode proKey", log.Any("proKey", proKey), log.Any("error", err))
+		return "", errors.Trace(err)
+	}
+
+	plaintext, err := sm4.CBCDecrypt(decKey, decIV, decCipherText)
+	if err != nil {
+		d.log.Error("failed to decrypt cipherText", log.Any("decCipherText", decCipherText), log.Any("error", err))
+		return "", errors.Trace(err)
+	}
+	return string(util.PKCS5UnPadding(plaintext)), nil
+}
+
+func XORBytes(a, b []byte) []byte {
+	if len(a) != len(b) {
+		return nil
+	}
+	buf := make([]byte, len(a))
+	for i := range a {
+		buf[i] = a[i] ^ b[i]
+	}
+	return buf
 }
 
 func (d *Decryption) Close() (err error) {
