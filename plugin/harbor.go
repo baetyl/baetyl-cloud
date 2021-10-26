9ce1e4986e0648ca885b3bc0f25293cef4e78eec
yibishufange <yibishufange@gmail.com>
2021-10-26 11:19:33 +0800

    fix uttest

--- plugin/harbor.go
+++ plugin/harbor.go
@@ -6,6 +6,8 @@ import (
 	"github.com/baetyl/baetyl-cloud/v2/common"
 )
 
+//go:generate mockgen -destination=../mock/plugin/harbor.go -package=plugin github.com/baetyl/baetyl-cloud/v2/plugin Harbor
+
 type Harbor interface {
 	GetImageDigest(c *common.Context) (string, error)
 	io.Closer
