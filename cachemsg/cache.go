c98dd06cea9e43cba18f83131ffc25430044a8ce
miaodanyang <miaodanyang@baidu.com>
2023-05-18 11:17:55 +0800

    node cache

--- cachemsg/cache.go
+++ cachemsg/cache.go
@@ -1,16 +1,6 @@
 package cachemsg
 
-import "fmt"
-
 const (
-	ShadowReportTImeCache = "shadow-%s-time"
-	ShadowReportCache     = "shadow-%s-report"
+	AllShadowReportTimeCache = "shadow-time"
+	AllShadowReportCache     = "shadow-report"
 )
-
-func GetShadowReportTimeCacheKey(nodeName string) string {
-	return fmt.Sprintf(ShadowReportTImeCache, nodeName)
-}
-
-func GetShadowReportCacheKey(nodeName string) string {
-	return fmt.Sprintf(ShadowReportCache, nodeName)
-}
