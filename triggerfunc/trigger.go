8fc62f6b67b15d93702791f8cb772d4f1c96fd25
miaow999 <kahdiuah@163.com>
2023-05-26 15:59:31 +0800

    fix cache lock (#570)
    * node cache update fix report cache too big

--- triggerfunc/trigger.go
+++ triggerfunc/trigger.go
@@ -16,33 +16,63 @@ const (
 	ShadowDelete                = "shadowDelete"
 )
 
+var (
+	reportSaveMap = map[string]reportSaveStruct{}
+	t             *time.Timer
+)
+
+// reportSaveStruct save temp need update data
+// @time control the updated content during high concurrency
+type reportSaveStruct struct {
+	Value string
+	Time  time.Time
+}
+
+func init() {
+	reportSaveMap = map[string]reportSaveStruct{}
+	t = time.NewTimer(time.Second)
+}
+
 // ShadowCreateOrUpdateCacheSet update shadow cache when shadow update or create
 func ShadowCreateOrUpdateCacheSet(cache plugin.DataCache, shadow models.Shadow) {
-	reportTimeData, err := cache.GetByte(cachemsg.AllShadowReportTimeCache)
-	if err != nil {
-		log.L().Error("get shadow cache error", log.Error(err))
-		return
+	reportSaveMap[shadow.Name] = reportSaveStruct{
+		Value: shadow.Time.Format(time.RFC3339Nano),
+		Time:  time.Now(),
 	}
-	reportTimeMap := map[string]string{}
-	err = json.Unmarshal(reportTimeData, &reportTimeMap)
-	if err != nil {
-		log.L().Error("unmarshal err", log.Error(err))
-		return
-	}
-	reportTimeMap[shadow.Name] = shadow.Time.Format(time.RFC3339Nano)
+	select {
+	case <-t.C:
+		defer func() {
+			t.Reset(time.Second)
+		}()
+		data, err := cache.GetString(cachemsg.CacheUpdateReportTimeLock)
+		if err != nil {
+			log.L().Error("update report  err", log.Error(err))
+		} else {
+			if data == "" {
+				err := cache.SetString(cachemsg.CacheUpdateReportTimeLock, time.Now().Format(time.RFC3339Nano))
+				if err != nil {
+					log.L().Error("update report  err", log.Error(err))
+				} else {
+					saveCache(cache, time.Now())
+					err = cache.Delete(cachemsg.CacheUpdateReportTimeLock)
+					if err != nil {
+						log.L().Error("update report  err", log.Error(err))
+					}
+				}
+			} else {
+				// lock key > 2 minute delete lock key
+				if time.Now().Add(-2*time.Minute).Format(time.RFC3339Nano) > data {
+					err = cache.Delete(cachemsg.CacheUpdateReportTimeLock)
+					if err != nil {
+						log.L().Error("update report  err", log.Error(err))
+					}
+				}
+			}
+		}
 
-	returnTimeData, err := json.Marshal(reportTimeMap)
-	if err != nil {
-		log.L().Error("Marshal err", log.Error(err))
-		return
+	default:
 	}
-	err = cache.SetByte(cachemsg.AllShadowReportTimeCache, returnTimeData)
-	if err != nil {
-		log.L().Error("set report time err", log.Error(err))
-		return
-	}
-
-	err = cache.SetByte(cachemsg.GetShadowReportCacheKey(shadow.Name), []byte(shadow.ReportStr))
+	err := cache.SetByte(cachemsg.GetShadowReportCacheKey(shadow.Name), []byte(shadow.ReportStr))
 	if err != nil {
 		log.L().Error("update report  err", log.Error(err))
 		return
@@ -54,6 +84,9 @@ func ShadowDeleteCache(cache plugin.DataCache, name string) {
 	reportTimeData, err := cache.GetByte(cachemsg.AllShadowReportTimeCache)
 	if err != nil {
 		log.L().Error("get shadow cache error", log.Error(err))
+		return
+	}
+	if reportTimeData == nil {
 		return
 	}
 	reportTimeMap := map[string]string{}
@@ -77,6 +110,39 @@ func ShadowDeleteCache(cache plugin.DataCache, name string) {
 	err = cache.Delete(cachemsg.GetShadowReportCacheKey(name))
 	if err != nil {
 		log.L().Error("delete report  err", log.Error(err))
+		return
+	}
+}
+
+func saveCache(cache plugin.DataCache, timeCheck time.Time) {
+	reportTimeData, err := cache.GetByte(cachemsg.AllShadowReportTimeCache)
+	if err != nil {
+		log.L().Error("get shadow cache error", log.Error(err))
+		return
+	}
+	reportTimeMap := map[string]string{}
+	if reportTimeData != nil {
+		err = json.Unmarshal(reportTimeData, &reportTimeMap)
+		if err != nil {
+			log.L().Error("unmarshal err", log.Error(err))
+			return
+		}
+	}
+	for key, val := range reportSaveMap {
+		//before timeCheck data update
+		if val.Time.Before(timeCheck) {
+			reportTimeMap[key] = val.Value
+			delete(reportSaveMap, key)
+		}
+	}
+	returnTimeData, err := json.Marshal(reportTimeMap)
+	if err != nil {
+		log.L().Error("Marshal err", log.Error(err))
+		return
+	}
+	err = cache.SetByte(cachemsg.AllShadowReportTimeCache, returnTimeData)
+	if err != nil {
+		log.L().Error("delete report time err", log.Error(err))
 		return
 	}
 }
