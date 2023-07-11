3684b93c31e969abc65a61f77c11b2738fedf834
miaow999 <kahdiuah@163.com>
2023-07-11 15:05:45 +0800

    fix default cache freecache (#599)

--- plugin/cache/localcache/cache_config.go
+++ plugin/cache/localcache/cache_config.go
@@ -1,10 +1,7 @@
 package localcache
 
-type FastCacheConfig struct {
-}
-
 type CloudConfig struct {
 	FreeCacheConfig struct {
-		MaxBytes int `json:"maxBytes" default:"10485760" yaml:"maxBytes"` //default 10 * 1024 * 1024  56m
-	} `yaml:"freeCacheConfig" json:"fastCacheConfig" default:"{}"`
+		MaxBytes int `json:"maxBytes" default:"10485760" yaml:"maxBytes"` //default 10 * 1024 * 1024  10m
+	} `yaml:"freeCacheConfig" json:"freeCacheConfig" default:"{}"`
 }
